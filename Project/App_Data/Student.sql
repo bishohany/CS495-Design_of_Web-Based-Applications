428f804eb96e84f655909ed2e3d33ee569d1fe7f
bishohany <81931887+bishohany@users.noreply.github.com>
2021-05-31 09:49:54 +0200

    week1
    database

---
+++ Project/App_Data/Student.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[StudentID] NVARCHAR(MAX) NOT NULL , 
+    [FNameA] NVARCHAR(MAX) NOT NULL, 
+    [MNameA] NVARCHAR(MAX) NOT NULL, 
+    [MMNameA] NVARCHAR(MAX) NOT NULL, 
+    [LNameA] NVARCHAR(MAX) NOT NULL, 
+    [BirthD] DATE NOT NULL, 
+    [FNameE] NVARCHAR(MAX) NOT NULL, 
+    [MNameE] NVARCHAR(MAX) NOT NULL, 
+    [MMNameE] NVARCHAR(MAX) NOT NULL, 
+    [LNameE] NVARCHAR(MAX) NOT NULL, 
+    [NaID] INT NOT NULL, 
+    [Certificate] NCHAR(3) NOT NULL, 
+    [Gender] NCHAR(1) NOT NULL, 
+    [Degree] FLOAT NOT NULL, 
+    PRIMARY KEY ([NaID])
+)
